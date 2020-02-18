538cbd1c6bb4464968354d7040d8961df00d77e7
OdileBiget <odile0903@gmail.com>
2020-02-18 16:03:44 +0100

    je sais utiliser github

--- src/main/java/servlets/Basic.java
+++ src/main/java/servlets/Basic.java
@@ -26,7 +26,7 @@ public class Basic extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
-		response.getWriter().append("Served at: ").append(request.getContextPath()).append("<br><h1>BASIC NOUVEAU 3</h1>");
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("<br><h1>JE SAIS UTILISER GITHUB</h1>");
 
 	}
 
