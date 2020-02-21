ebd6da5b7c85df37af86b97292b34f63caa65eed
OdileBiget <odile0903@gmail.com>
2020-02-21 16:52:45 +0100

    carte

--- src/main/java/servlets/MapGarden.java
+++ src/main/java/servlets/MapGarden.java
@@ -3,75 +3,71 @@ package servlets;
 import java.io.IOException;
 import java.util.List;
 
-import javax.json.Json;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.*;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.google.gson.Gson;
+
 import beans.JardinProfil;
-import beans.Profil;
 
 /**
  * Servlet implementation class MapGarden
  */
 public class MapGarden extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MapGarden() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public MapGarden() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 
 		// Initiliation
-				SessionFactory factory = new Configuration().configure().buildSessionFactory();
-				Session session = factory.openSession();
+		SessionFactory factory = new Configuration().configure().buildSessionFactory();
+		Session session = factory.openSession();
 
-				session.beginTransaction();
-				Query query = session.createQuery("FROM JardinProfil");
+		session.beginTransaction();
+		Query query = session.createQuery("FROM JardinProfil");
 
-				List<JardinProfil> result = query.list();
-				for (JardinProfil p : (List<JardinProfil>) result) {
+		List<JardinProfil> result = query.list();
 
-					String json = Json.createObjectBuilder()
-				            .add("adresse", p.getAdresse())
-				            .add("villeNom", p.getCodePostal())
-				            .add("codePostal", p.getCodePostal())
-				            .add("coordonneesGPS", p.getCoordonneesGPS())
-				            .add("typeJardin", p.getTypeJardin())
-				            .add("superficie", p.getSuperficie())
-				            .add("typeCulture", p.getTypeCulture())
-//				            .add("culturesPresentes", p.getCulturesPresentes())
-				            .add("typeSol", p.getTypeSol())
-				            .add("typeProduction", p.getTypeProduction())
-				            //.add("cheminImages", p.getCheminImages())
-				            .build()
-				            .toString();
-				}
+		String json = new Gson().toJson(result);
 
-				session.getTransaction().commit();
+		System.out.println(json);
+		request.setAttribute("json", json);
+		session.getTransaction().commit();
 
-				session.close();
 		
+		session.close();
+		this.getServletContext().getRequestDispatcher("/mapGardens.jsp").forward(request, response);
+
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
