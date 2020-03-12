b640774803e95b9cb3d504e9de9f36c5c9d2b1f7
DavidLeGuern <david.leguern@hotmail.fr>
2020-03-12 14:50:47 +0100

    maj dav jeudi

--- src/main/webapp/js/jardin.js
+++ src/main/webapp/js/jardin.js
@@ -71,30 +71,49 @@ function decodeUTF8(string) {
 // Get the modal
 var modal = document.getElementById("myModal");
 
+var modalSupp = document.getElementById("myModalSupp");
+
 // Get the button that opens the modal
 var btn = document.getElementById("btn-ajoutImage");
+var btnSupp = document.getElementById("btn-suppImage");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//var span = document.getElementsByClassName("close ajout")[0];
+//
+//var spanSupp = document.getElementsByClassName("close suppression")[0];
+
+var span = document.getElementById("closeAjout");
+
+var spanSupp = document.getElementById("closeSupp");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
 	modal.style.display = "block";
+}
+btnSupp.onclick = function () {
+	modalSupp.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
 	modal.style.display = "none";
 }
-
+spanSupp.onclick = function () {
+	
+	modalSupp.style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
 	if (event.target == modal) {
 		modal.style.display = "none";
+		
+		
+	}
+	if (event.target == modalSupp){
+		modalSupp.style.display = "none";
 	}
 }
 //--------------------------------------------------------------------------------------------------
-
 
 
 
